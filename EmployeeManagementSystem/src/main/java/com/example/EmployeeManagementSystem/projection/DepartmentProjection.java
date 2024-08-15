e4b3caaaf9150ecaa791a884ea1e8d7c12447c6b
RISHIK-CHITTA <132035888+RISHIK-CHITTA@users.noreply.github.com>
2024-08-15 17:43:15 +0530

    Delete EmployeeManagementSystem directory

--- EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/projection/DepartmentProjection.java
+++
@@ -1,6 +0,0 @@
-package com.example.employeemanagementsystem.projection;
-
-public interface DepartmentProjection {
-    Long getDepartmentId();
-    String getDepartmentName();
-}
--- EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/projection/EmployeeProjection.java
+++
@@ -1,7 +0,0 @@
-package com.example.employeemanagementsystem.projection;
-
-public interface EmployeeProjection {
-    Long getEmployeeId();
-    String getFullName();
-    String getEmailAddress();
-}
